4be75b522c654c49f5a0b8bcff9869f80f6ee052
Inhoon <inhuns@gmail.com>
2025-03-18 00:18:17 +0900

    Update matplot.py

--- matplot.py
+++ matplot.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    img_file = "img/Lenna.png"  # 표시할 이미지 경로 ---①
+    img_file = "img/Lena.png"  # 표시할 이미지 경로 ---①
     # img = cv2.imread(img_file)  # 이미지를 읽어서 img 변수에 할당 ---②
     img = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)  # 회색으로 읽기
     img_color = cv2.imread(img_file, cv2.IMREAD_COLOR)  # 컬러영상 읽기
